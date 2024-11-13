49b1b0df444196fe6f7db1add2de80589ad4dd50
axelklaudedelgado <144205486+axelklaudedelgado@users.noreply.github.com>
2024-11-13 11:44:24 +0800

    fix: use static to use dist folder

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 app.use(cors({ origin: 'http://localhost:5173' }))
 app.use(express.json())
+app.use(express.static('dist'))
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
